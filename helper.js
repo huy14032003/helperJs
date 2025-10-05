6bccc21270408c8668782a8563fc7168420fe440
huy14032003 <trhuy14032003@gmail.com>
2025-10-05 18:13:37 +0700

    update helper

--- helper.js
+++ helper.js
@@ -1,3 +1,11 @@
+
+
+/**
+ * 
+ * @param {function} fn function xử lý search vd deepSearch
+ * @param {int} delay time delay
+ * @returns 
+ */
 export function debounce(fn, delay = 300) {
   let timeoutId;
   return function (...args) {
@@ -5,7 +13,12 @@ export function debounce(fn, delay = 300) {
     timeoutId = setTimeout(() => fn.apply(this, args), delay);
   };
 }
-
+/**
+ * 
+ * @param {object} obj chuỗi obj cần search
+ * @param {*} searchTerm giá trị search, thường là value của input
+ * @returns 
+ */
 export function deepSearch(obj, searchTerm) {
   let stack = [obj];
 
@@ -14,18 +27,12 @@ export function deepSearch(obj, searchTerm) {
     for (let key in current) {
       if (Object.prototype.toString.call(current[key]) === "[object Object]") {
         stack.push(current[key]);
-      } else if (
-        Object.prototype.toString.call(current[key]) === "[object Array]"
-      ) {
+      } else if (Object.prototype.toString.call(current[key]) === "[object Array]") {
         current[key].forEach((item) => stack.push(item));
       } else {
         if (
           current[key] &&
-          current[key]
-            .toString()
-            .toLowerCase()
-            .trim()
-            .indexOf(searchTerm.toString().toLowerCase().trim()) > -1
+          current[key].toString().toLowerCase().trim().indexOf(searchTerm.toString().toLowerCase().trim()) > -1
         ) {
           return true;
         }
@@ -34,15 +41,35 @@ export function deepSearch(obj, searchTerm) {
   }
   return false;
 }
+/**
+ * 
+ * @param {number} input giá trị cần format
+ */
 export function allowOnlyNumbers(input) {
-   input.addEventListener('input', () => {
-   input.value = input.value.replace(/[^0-9.]/g, '');
- });
+  input.addEventListener("input", () => {
+    input.value = input.value.replace(/[^0-9.]/g, "");
+  });
 }
-
+/**
+ * 
+ * @param {value} value giá trị cần format
+ * @returns 
+ */
 export function parseCurrency(value) {
   if (!value) return 0;
-      let cleaned = value.toString().replace(/[^\d]/g, "");
-          if (cleaned === "") return 0;
+  let cleaned = value.toString().replace(/[^\d]/g, "");
+  if (cleaned === "") return 0;
   return parseInt(cleaned, 10);
+}
+
+/**
+ * 
+ * @param {object} data data cần filter
+ * @param {string} type loại desc lớn => nhỏ hoặc asc nhỏ => lớn
+ * @returns 
+ */
+export function sortObj(data, type = "desc") {
+  const arr = Object.entries(data);
+  const sorted = arr.sort((a, b) => (type === "asc" ? a[1] - b[1] : b[1] - a[1]));
+  return sorted.map(([key, value]) => ({ key, value }));
 }
